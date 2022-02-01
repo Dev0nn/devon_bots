af81630468ab4b1759b02cbdfce1942ee8afeedb
Dev0nn <98854530+Dev0nn@users.noreply.github.com>
2022-02-02 01:52:45 +0300

    Create channelDB.js

---
+++ backup_bot/channelDB.js
@@ -0,0 +1,10 @@
+ 
+ ​const​ ​mongoose​ ​=​ ​require​(​"mongoose"​)​; 
+  
+ ​const​ ​dark_channelDB​ ​=​ ​mongoose​.​Schema​(​{ 
+ ​  ​roleid​: ​String​, 
+ ​  ​rolename​: ​String​, 
+ ​  ​channels​: ​Array 
+ ​}​)​; 
+  
+ ​module​.​exports​ ​=​ ​mongoose​.​model​(​"dark_channelDB"​,​ ​dark_channelDB​)​;
