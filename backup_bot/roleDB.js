c8c120124af465fd227fae3d3aa937b671f8edc9
Dev0nn <98854530+Dev0nn@users.noreply.github.com>
2022-02-02 01:51:22 +0300

    Create roleDB.js

---
+++ backup_bot/roleDB.js
@@ -0,0 +1,17 @@
+ 
+ ​const​ ​mongoose​ ​=​ ​require​(​"mongoose"​)​; 
+  
+ ​const​ ​dark_rol​ ​=​ ​mongoose​.​Schema​(​{ 
+ ​  ​_id​: ​mongoose​.​Schema​.​Types​.​ObjectId​, 
+ ​  ​rolid​: ​String​, 
+ ​  ​name​: ​String​, 
+ ​  ​color​: ​String​, 
+ ​  ​members​: ​Array​, 
+ ​  ​permissions​: ​Number​, 
+ ​  ​position​: ​Number​, 
+ ​  ​hoisted​: ​Boolean​, 
+ ​  ​time​: ​Number​, 
+ ​  ​sayı​: ​Number​, 
+ ​}​)​; 
+  
+ ​module​.​exports​ ​=​ ​mongoose​.​model​(​"dark_rol"​,​ ​dark_rol​)​;
