980f3b5c9f060f0574c2d71a1b7f7c4df8dcbe52
Dev0nn <98854530+Dev0nn@users.noreply.github.com>
2022-02-02 14:19:31 +0300

    Update channelDB.js

--- backup_bot/channelDB.js
+++ backup_bot/channelDB.js
@@ -1,10 +1,9 @@
- 
- ​const​ ​mongoose​ ​=​ ​require​(​"mongoose"​)​; 
-  
- ​const​ ​dark_channelDB​ ​=​ ​mongoose​.​Schema​(​{ 
- ​  ​roleid​: ​String​, 
- ​  ​rolename​: ​String​, 
- ​  ​channels​: ​Array 
- ​}​)​; 
-  
- ​module​.​exports​ ​=​ ​mongoose​.​model​(​"dark_channelDB"​,​ ​dark_channelDB​)​;
+const mongoose = require("mongoose");
+
+const dark_channelDB = mongoose.Schema({
+  roleid: String,
+  rolename: String,
+  channels: Array
+});
+
+module.exports = mongoose.model("dark_channelDB", dark_channelDB);
