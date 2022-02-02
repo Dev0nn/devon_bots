7075fc41366a9a8a9613825130952ddd5153ab75
Dev0nn <98854530+Dev0nn@users.noreply.github.com>
2022-02-02 14:18:50 +0300

    Update roleDB.js

--- backup_bot/roleDB.js
+++ backup_bot/roleDB.js
@@ -1,17 +1,16 @@
- 
- ​const​ ​mongoose​ ​=​ ​require​(​"mongoose"​)​; 
-  
- ​const​ ​dark_rol​ ​=​ ​mongoose​.​Schema​(​{ 
- ​  ​_id​: ​mongoose​.​Schema​.​Types​.​ObjectId​, 
- ​  ​rolid​: ​String​, 
- ​  ​name​: ​String​, 
- ​  ​color​: ​String​, 
- ​  ​members​: ​Array​, 
- ​  ​permissions​: ​Number​, 
- ​  ​position​: ​Number​, 
- ​  ​hoisted​: ​Boolean​, 
- ​  ​time​: ​Number​, 
- ​  ​sayı​: ​Number​, 
- ​}​)​; 
-  
- ​module​.​exports​ ​=​ ​mongoose​.​model​(​"dark_rol"​,​ ​dark_rol​)​;
+const mongoose = require("mongoose");
+
+const dark_rol = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  rolid: String,
+  name: String,
+  color: String,
+  members: Array,
+  permissions: Number,
+  position: Number,
+  hoisted: Boolean,
+  time: Number,
+  sayı: Number,
+});
+
+module.exports = mongoose.model("dark_rol", dark_rol);
